5ac5b2eb74a1c1714578c62804e9d4a77fcd75a2
Cabdikariim <cabdikariimmaxamaddaahir@gmail.com>
2025-12-30 13:14:54 +0300

    finishing the setup

---
+++ app/Auth/singUp.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const singUp = () => {
+  return (
+      <View className="flex justify-center items-center h-full bg-red-300 "
+    >
+      <Text className="text-green-500 text-xl  ">Welcome to Sing up Page</Text>
+    </View>
+  )
+}
+
+export default singUp
